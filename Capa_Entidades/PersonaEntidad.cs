96eaecfccd0a69defd7d2faf5d56e57d6c758f44
Gosvemusic <jarctoni15@gmail.com>
2025-02-19 12:38:04 -0600

    Update PersonaEntidad.cs
    cambios en Persona Entidad

--- Capa_Entidades/PersonaEntidad.cs
+++ Capa_Entidades/PersonaEntidad.cs
@@ -1,12 +1,21 @@
 ﻿namespace Capa_Entidades // Espacio para organizar clases que se relacionan con entidades del sistema
 {
-    public class PersonaEntidad // Clase que representa una persona en el sistema
+    // Clase base para administradores y clientes
+    public class PersonaEntidad
     {
-        public int Identificacion { get; set; } // Propiedad para el ID de la persona
-        public string Nombre { get; set; } = string.Empty; // Nombre de la persona, inicializado vacio
+        // Identificacion unica de la persona
+        public int Identificacion { get; set; }
 
-        public string PrimerApellido { get; set; } = string.Empty; // Primer apellido, inicializado vacio
-        public string SegundoApellido { get; set; } = string.Empty; // Segundo apellido, inicializado vacio
-        public DateTime FechaNacimiento { get; set; } // Fecha de nacimiento de la persona
+        // Nombre de la persona
+        public required string Nombre { get; set; }
+
+        // Primer apellido de la persona
+        public required string PrimerApellido { get; set; }
+
+        // Segundo apellido de la persona
+        public required string SegundoApellido { get; set; }
+
+        // Fecha de nacimiento
+        public DateTime FechaNacimiento { get; set; }
     }
 }
