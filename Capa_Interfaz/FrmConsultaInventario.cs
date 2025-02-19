eca28ce79602aba8620295358ea3d46b85a016b9
Gosvemusic <jarctoni15@gmail.com>
2025-02-19 13:13:36 -0600

    Update FrmConsultaInventario.cs
    nuevos cambios en FrmConsultaInventario

--- Capa_Interfaz/FrmConsultaInventario.cs
+++ Capa_Interfaz/FrmConsultaInventario.cs
@@ -10,11 +10,37 @@ using System.Windows.Forms;
 
 namespace Capa_Interfaz
 {
+    // Formulario para consultar el inventario
     public partial class FrmConsultaInventario : Form
     {
-        public FrmConsultaInventario()
+        private VideojuegosXTiendaEntidad[] inventario;
+
+        public FrmConsultaInventario(VideojuegosXTiendaEntidad[] inventario)
         {
             InitializeComponent();
+            this.inventario = inventario;
+        }
+
+        // Evento para cargar los datos en el DataGridView
+        private void btnCargarDatos_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dgvInventario.DataSource = inventario.Where(i => i != null).Select(i => new 
+                {
+                    TiendaID = i.Tienda.Id,
+                    TiendaNombre = i.Tienda.Nombre,
+                    TiendaDireccion = i.Tienda.Direccion,
+                    VideojuegoID = i.Videojuego.Id,
+                    VideojuegoNombre = i.Videojuego.Nombre,
+                    TipoVideojuego = i.Videojuego.TipoVideojuego.Nombre,
+                    Existencias = i.Existencias
+                }).ToList();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar los datos: " + ex.Message);
+            }
         }
     }
 }
