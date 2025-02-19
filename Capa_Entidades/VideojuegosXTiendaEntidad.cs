e8acf08f4a2b87e97f40021b2207e9963fd19ca6
Gosvemusic <jarctoni15@gmail.com>
2025-02-19 12:46:31 -0600

    Update VideojuegosXTiendaEntidad.cs
    cambios en VideojuegosxtiendaEntidad

--- Capa_Entidades/VideojuegosXTiendaEntidad.cs
+++ Capa_Entidades/VideojuegosXTiendaEntidad.cs
@@ -1,14 +1,10 @@
 ﻿namespace Capa_Entidades // Espacio para organizar clases que se relacionan con entidades del sistema
 {
-    public class VideojuegosXTiendaEntidad  // Clase que representa una videojuego por tienda en el sistema, con acceso interno
+    // Clase que representa la asociacion entre videojuegos y tiendas
+    public class VideojuegosXTiendaEntidad
     {
-        public TiendaEntidad Tienda { get; set; } // Tienda donde esta disponible el videojuego (relacion con TiendaEntidad)
-        public VideojuegoEntidad Videojuego { get; set; } // Videojuego disponible en la tienda (relacion con VideojuegoEntidad)
-        public int Existencias { get; set; } // Cantidad de existencias del videojuego en la tienda
-
-
-
-
-
+        public required TiendaEntidad Tienda { get; set; } // Tienda donde se encuentra el videojuego
+        public required VideojuegoEntidad Videojuego { get; set; } // Videojuego disponible en la tienda
+        public int Existencias { get; set; } // Cantidad de copias disponibles en la tienda
     }
 }
