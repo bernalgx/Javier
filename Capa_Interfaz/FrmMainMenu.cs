d206311eb397e568acf858a4ff1e8d776017044d
Gosvemusic <jarctoni15@gmail.com>
2025-02-19 13:05:25 -0600

    Update FrmMainMenu.cs
    nuevos cambios en FrmMainMenu

--- Capa_Interfaz/FrmMainMenu.cs
+++ Capa_Interfaz/FrmMainMenu.cs
@@ -10,11 +10,47 @@ using System.Windows.Forms;
 
 namespace Capa_Interfaz
 {
-    public partial class FrmMainMenu : Form
+    // Formulario principal de la aplicacion
+    public partial class FrmMenuPrincipal : Form
     {
-        public FrmMainMenu()
+        public FrmMenuPrincipal()
         {
             InitializeComponent();
+        }
+
+        // Evento para abrir el formulario de administradores
+        private void btnAdministradores_Click(object sender, EventArgs e)
+        {
+            FrmRegistrarAdministrador form = new FrmRegistrarAdministrador();
+            form.ShowDialog();
+        }
+
+        // Evento para abrir el formulario de clientes
+        private void btnClientes_Click(object sender, EventArgs e)
+        {
+            FrmRegistrarCliente form = new FrmRegistrarCliente();
+            form.ShowDialog();
+        }
+
+        // Evento para abrir el formulario de tiendas
+        private void btnTiendas_Click(object sender, EventArgs e)
+        {
+            FrmRegistrarTienda form = new FrmRegistrarTienda();
+            form.ShowDialog();
+        }
+
+        // Evento para abrir el formulario de videojuegos
+        private void btnVideojuegos_Click(object sender, EventArgs e)
+        {
+            FrmRegistrarVideojuego form = new FrmRegistrarVideojuego();
+            form.ShowDialog();
+        }
+
+        // Evento para abrir el formulario de inventario
+        private void btnInventario_Click(object sender, EventArgs e)
+        {
+            FrmConsultarInventario form = new FrmConsultarInventario();
+            form.ShowDialog();
         }
     }
 }
