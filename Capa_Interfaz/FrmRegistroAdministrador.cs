692c782703191a0c9b1b13296c808de17c9b10ad
Gosvemusic <jarctoni15@gmail.com>
2025-02-19 18:10:25 -0600

    Update FrmRegistroAdministrador.cs
    comente FrmRegistroAdministrador

--- Capa_Interfaz/FrmRegistroAdministrador.cs
+++ Capa_Interfaz/FrmRegistroAdministrador.cs
@@ -1,56 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+//using System.Drawing;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Windows.Forms;
 
-namespace Capa_Interfaz
-{
+//namespace Capa_Interfaz
+//{
     // Formulario para registrar administradores
-    public partial class FrmRegistroAdministrador : Form
-    {
-        private LogAdministrador logica = new LogicaAdministrador();
+//    public partial class FrmRegistroAdministrador : Form
+//    {
+//        private LogAdministrador logica = new LogicaAdministrador();
 
-        public FrmRegistroAdministrador()
-        {
-            InitializeComponent();
-        }
+//        public FrmRegistroAdministrador()
+ //       {
+ //           InitializeComponent();
+//        }
 
         // Evento del boton para registrar un administrador
-        private void btnRegistrar_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                int identificacion = int.Parse(txtIdentificacion.Text);
-                string nombre = txtNombre.Text;
-                string primerApellido = txtPrimerApellido.Text;
-                string segundoApellido = txtSegundoApellido.Text;
-                DateTime fechaNacimiento = dtpFechaNacimiento.Value;
-                DateTime fechaContratacion = dtpFechaContratacion.Value;
+//        private void btnRegistrar_Click(object sender, EventArgs e)
+ //       {
+ //           try
+ //           {
+//                int identificacion = int.Parse(txtIdentificacion.Text);
+ //               string nombre = txtNombre.Text;
+//                string primerApellido = txtPrimerApellido.Text;
+//                string segundoApellido = txtSegundoApellido.Text;
+//                DateTime fechaNacimiento = dtpFechaNacimiento.Value;
+//                DateTime fechaContratacion = dtpFechaContratacion.Value;
 
-                string mensaje = logica.RegistroAdministrador(identificacion, nombre, primerApellido, segundoApellido, fechaNacimiento, fechaContratacion);
-                MessageBox.Show(mensaje);
+//                string mensaje = logica.RegistroAdministrador(identificacion, nombre, primerApellido, segundoApellido, fechaNacimiento, fechaContratacion);
+//                MessageBox.Show(mensaje);
 
                 // Limpiar los campos despues de registrar
-                txtIdentificacion.Clear();
-                txtNombre.Clear();
-                txtPrimerApellido.Clear();
-                txtSegundoApellido.Clear();
-                dtpFechaNacimiento.Value = DateTime.Today;
-                dtpFechaContratacion.Value = DateTime.Today;
-            }
-            catch (FormatException)
-            {
-                MessageBox.Show("Error: Ingrese valores numéricos en la identificación.");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error inesperado: " + ex.Message);
-            }
-        }
-    }
-}
+ //               txtIdentificacion.Clear();
+ //               txtNombre.Clear();
+ //               txtPrimerApellido.Clear();
+ //               txtSegundoApellido.Clear();
+ //               dtpFechaNacimiento.Value = DateTime.Today;
+ //               dtpFechaContratacion.Value = DateTime.Today;
+ //           }
+ //           catch (FormatException)
+ //           {
+ //               MessageBox.Show("Error: Ingrese valores numéricos en la identificación.");
+ //           }
+  //          catch (Exception ex)
+ //           {
+ //               MessageBox.Show("Error inesperado: " + ex.Message);
+ //           }
+ //       }
+//    }
+//}
