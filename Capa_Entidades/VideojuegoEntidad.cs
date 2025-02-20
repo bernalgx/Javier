3b553bd0bcdfcaf215f3b6e551b4e51e2f6f8ba9
Gosvemusic <jarctoni15@gmail.com>
2025-02-20 10:36:59 -0600

    Update VideojuegoEntidad.cs
    cambios en VideojuegoEntidad, Implementación del CRUD en la Entidad

--- Capa_Entidades/VideojuegoEntidad.cs
+++ Capa_Entidades/VideojuegoEntidad.cs
@@ -1,12 +1,22 @@
 ﻿namespace Capa_Entidades // Espacio para organizar clases que se relacionan con entidades del sistema
 {
-	public class VideojuegoEntidad // Clase que representa una videojuego en el sistema, con acceso interno
-	{
-		public int Id { get; set; } // Identificador unico del videojuego
-		public string Nombre { get; set; } = string.Empty;// Nombre del videojuego
-		public int TipoVideojuego { get; set; }// Tipo de videojuego (relacion con TipoVideojuegoEntidad)
-		public string Desarrollador { get; set; } = string.Empty;// Nombre de la empresa desarrolladora del videojuego
-		public int Lanzamiento { get; set; }  // Año de lanzamiento del videojuego
-		public bool Fisico { get; set; } // Indica si el videojuego es fisico (true) o digital (false)
-	}
+    public class VideojuegoEntidad
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public TipoVideojuegoEntidad TipoVideojuego { get; set; }
+        public string Desarrollador { get; set; }
+        public int Lanzamiento { get; set; }
+        public bool Fisico { get; set; } // True = Fisico, False = Virtual
+
+        public VideojuegoEntidad(int id, string nombre, TipoVideojuegoEntidad tipoVideojuego, string desarrollador, int lanzamiento, bool fisico)
+        {
+            Id = id;
+            Nombre = nombre;
+            TipoVideojuego = tipoVideojuego;
+            Desarrollador = desarrollador;
+            Lanzamiento = lanzamiento;
+            Fisico = fisico;
+        }
+    }
 }
